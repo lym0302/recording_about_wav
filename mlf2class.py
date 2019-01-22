fc1ecebf173bee793c3f90b3f7e9e76ffd931f99
lym0302 <lym0302@foxmail.com>
2019-01-22 07:43:57 +0000

    mlf2class

---
+++ mlf2class.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+
+import os
+import re
+import sys
+from os import path
+
+
+if __name__ == "__main__":
+
+  inwav_dir = sys.argv[1]
+  outwav_dir = sys.argv[2]
+  mlf_filename = sys.argv[3]
+  raw_number = 0
+  phone_number = 0
+  d = dict()
+  for line in open(mlf_filename,encoding='utf-8').readlines():
+    if 'lab' in line:
+      filename = line.split('*/')[1].split('.')[0] 
+      input_filename = inwav_dir + filename + '.wav'
+      print(input_filename)
+      raw_number += 1
+    elif ('MLF'  not  in line) and (not line.startswith('.')):
+      phone_number += 1
+      start = float(line.split(' ')[0])/10000000.0
+      end = float(line.split(' ')[1])/10000000.0
+      length = end - start
+      phoneme = line.split(' ')[2]
+      if phoneme in d:
+        d[phoneme] += 1
+      else:
+        d[phoneme] = 1
+      save_dir = outwav_dir + phoneme + "/"
+      if not path.exists(save_dir):
+        os.makedirs(save_dir)
+      number = line.split(' ')[3]
+      
+      output_filename = save_dir + filename + '_' + 'detail'+ '_' + str(start) + '_' + str(end) + '_' + phoneme + '.wav'
+      os.system('sox %s %s trim  %f %f'%(input_filename, output_filename, start, length))
+      #print(type(phoneme))
+      #print("cp %s %s " %(input_filename, save_dir))
+  print(d)
+   #   output_filename = outwav_dir + filename + '_' + 'detail'+ '_' + str(start) + '_' + str(end) + '_' + phoneme + '.wav'
+   #   os.system('sox %s %s trim  %f %f'%(input_filename, output_filename, start, length))
+   #   os.system("cp %s %s " %(input_filename, outwav_dir))
+  print("the total number of raw wav in this mlf is : %d \n" %(raw_number) )
+  print("the total number of phone wav in this mlf is : %d \n" %(phone_number) )
